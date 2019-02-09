e4d550004c83eae8dbfc65f53ce2b0aaf9eae9c3
Yan BUATOIS <dracoctix@outlook.fr>
2019-02-09 20:47:37 +0100

    On se connecte maintenant à socket.io.

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const electron = require('electron');
 const {BrowserWindow, app, ipcMain} = electron;
 const API = require('./class/API');
 const config = require('./config');
-const io = require('socket.io');
+const io = require('socket.io-client');
 
 let mainWindow;
 const iqApi = new API(`${config.apiUrl}:${config.apiPort}`);
+const socket = io(`${config.apiUrl}:${config.apiPort}`);
 
 /**
  * Permet de créer la fenêtre.
@@ -29,6 +30,7 @@ app.on('ready', createWindow);
 ipcMain.on('login-submit', async (event, arg) => {
   try {
     const token = await iqApi.login(arg);
+    socket.emit('login', token);
   }
   catch(err) {
     event.sender.send('login-error', (err.message));
